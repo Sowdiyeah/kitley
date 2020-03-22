254c54bde1fef0999f66bf4fef39e1944beeddf0
Menno <menno1337@gmail.com>
2020-03-22 12:12:12 +0100

    Add onTap callback to navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,21 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  int _selectedIndex = 0;
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text('Index 0: Home'),
+    Text('Index 1: Business'),
+    Text('Index 2: School'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,14 +28,21 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Center(child: Text('Kitley')),
         ),
+        body: _widgetOptions[_selectedIndex],
         bottomNavigationBar: BottomNavigationBar(
-          items: [
+          items: <BottomNavigationBarItem>[
             BottomNavigationBarItem(icon: Icon(Icons.search), title: Text('Search')),
             // BottomNavigationBarItem(icon: Icon(Icons.perm_identity), title: Text('Profile')),
             BottomNavigationBarItem(icon: Icon(Icons.gavel), title: Text('Borrow')),
             // BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), title: Text('Inventory')),
             BottomNavigationBarItem(icon: Icon(Icons.question_answer), title: Text('Chat')),
           ],
+          onTap: (int index) {
+            setState(() {
+              _selectedIndex = index;
+            });
+          },
+          currentIndex: _selectedIndex,
         ),
       ),
     );
