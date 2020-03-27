38df2e555ec52d6db05c64b06e9a8e6aa56974ec
Menno <menno1337@gmail.com>
2020-03-27 18:44:29 +0100

    Boilerplate for add item

---
+++ lib/pages/add_item.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AddItemPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Add Item')),
+      body: Text('ProfilePage app goes here'),
+    );
+  }
+}
