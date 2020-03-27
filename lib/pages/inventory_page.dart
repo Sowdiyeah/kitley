f2035dfae8afc64204ba19fa8141278950451719
Menno <menno1337@gmail.com>
2020-03-27 18:42:43 +0100

    Add button to add an item

--- lib/pages/inventory_page.dart
+++ lib/pages/inventory_page.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:kitley/pages/add_item.dart';
 
 class InventoryPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text('InventoryPage app goes here');
+    return Center(
+      child: RaisedButton(
+        child: Text('Add item'),
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => AddItemPage()));
+        },
+      ),
+    );
   }
 }
