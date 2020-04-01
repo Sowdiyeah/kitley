4ce67c7e8f5fec0e0b645eb1d4a6fbc84ec07cd3
Menno <menno1337@gmail.com>
2020-04-01 18:25:41 +0200

    Rename item_template.dart to item.dart

--- lib/pages/add_item.dart
+++ lib/pages/add_item.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 
-import 'package:kitley/template/item_template.dart';
+import 'package:kitley/template/item.dart';
 
 class AddItemPage extends StatefulWidget {
   @override
