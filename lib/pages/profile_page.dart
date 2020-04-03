86c703f862c22addd1a38161218ffc36201f6326
Menno <menno1337@gmail.com>
2020-04-03 16:23:30 +0200

    Add the ability to sign in with Google

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,11 +1,58 @@
 import 'package:flutter/material.dart';
 
-class ProfilePage extends StatelessWidget {
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class ProfilePage extends StatefulWidget {
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('My Profile')),
-      body: Text('ProfilePage app goes here'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              child: Text('Log-in'),
+              onPressed: () {
+                _handleSignIn();
+              },
+            ),
+            RaisedButton(
+              child: Text('Log-Out'),
+              onPressed: () {
+                _handleSignOut();
+              },
+            ),
+          ],
+        ),
+      ),
     );
+  }
+
+  Future<FirebaseUser> _handleSignIn() async {
+    GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+    GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+
+    AuthCredential credential = GoogleAuthProvider.getCredential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    FirebaseUser user = (await _auth.signInWithCredential(credential)).user;
+    print("signed in " + user.displayName);
+    return user;
+  }
+
+  void _handleSignOut() {
+    _googleSignIn.signOut();
   }
 }
