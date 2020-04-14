aab81ae045202c03781771dec0125f467426cdf8
Menno <menno1337@gmail.com>
2020-04-14 15:39:33 +0200

    Build the drawer profile with FirebaseUser info

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,13 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:kitley/pages/add_item_page.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
+import 'package:kitley/pages/add_item_page.dart';
 import 'package:kitley/pages/chat_page.dart';
 import 'package:kitley/pages/inventory_page.dart';
-import 'package:kitley/pages/profile_page.dart';
+import 'package:kitley/pages/login_page.dart';
 import 'package:kitley/pages/search_page.dart';
 import 'package:kitley/template/search_delegate.dart';
 
@@ -91,6 +95,7 @@ class _Drawer extends StatefulWidget {
 }
 
 class _DrawerState extends State<_Drawer> {
+  Future<FirebaseUser> _user = FirebaseAuth.instance.currentUser();
   bool _showUserDetails = false;
 
   @override
@@ -98,7 +103,7 @@ class _DrawerState extends State<_Drawer> {
     return Drawer(
       child: Column(
         children: <Widget>[
-          _buildDrawerHeader(context),
+          _buildDrawerHeader(),
           Expanded(
             child: _showUserDetails ? _buildUserDetail() : _buildDrawerList(),
           ),
@@ -107,13 +112,48 @@ class _DrawerState extends State<_Drawer> {
     );
   }
 
+  Widget _buildDrawerHeader() {
+    return FutureBuilder(
+      future: _user,
+      builder: (_, AsyncSnapshot<FirebaseUser> snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting:
+            return Center(child: Text('Loading....'));
+          default:
+            if (snapshot.hasError) return Text('Error: ${snapshot.error}');
+            return UserAccountsDrawerHeader(
+              arrowColor: Colors.black,
+              accountName: Text(snapshot.data.displayName),
+              accountEmail: Text(snapshot.data.email),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage:
+                    CachedNetworkImageProvider(snapshot.data.photoUrl),
+              ),
+              onDetailsPressed: () {
+                setState(() {
+                  _showUserDetails = !_showUserDetails;
+                });
+              },
+            );
+        }
+      },
+    );
+  }
+
   ListView _buildUserDetail() {
     return ListView(
       padding: EdgeInsets.zero,
       children: <Widget>[
         ListTile(
-          leading: Icon(Icons.exit_to_app),
-          title: Text('Log in'),
+          leading: FaIcon(FontAwesomeIcons.signOutAlt),
+          title: Text('Log out'),
+          onTap: () async {
+            await FirebaseAuth.instance.signOut();
+            await GoogleSignIn().signOut();
+            Navigator.of(context).pushReplacement(
+              MaterialPageRoute(builder: (context) => LoginPage()),
+            );
+          },
         ),
       ],
     );
@@ -139,39 +179,10 @@ class _DrawerState extends State<_Drawer> {
           onTap: () {},
         ),
         ListTile(
-          leading: Icon(Icons.account_circle),
-          title: Text('Profile'),
-          onTap: () {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => ProfilePage()));
-          },
-        ),
-        ListTile(
           leading: Icon(Icons.settings),
           title: Text('Settings'),
         ),
       ],
-    );
-  }
-
-  UserAccountsDrawerHeader _buildDrawerHeader(BuildContext context) {
-    return _anonymousDrawerHeader();
-  }
-
-  UserAccountsDrawerHeader _anonymousDrawerHeader() {
-    return UserAccountsDrawerHeader(
-      arrowColor: Colors.black,
-      accountName: Text('Anonymous'),
-      accountEmail: Text(''),
-      currentAccountPicture: CircleAvatar(
-        backgroundColor: Colors.brown.shade800,
-        child: Text('A', style: TextStyle(fontSize: 36)),
-      ),
-      onDetailsPressed: () {
-        setState(() {
-          _showUserDetails = !_showUserDetails;
-        });
-      },
     );
   }
 }
