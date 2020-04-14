9e6d7fa48046a8090632e766ee1d257602a0ecc3
Menno <menno1337@gmail.com>
2020-04-14 17:35:06 +0200

    Make loggin in more robust

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 import 'package:kitley/pages/home_page.dart';
@@ -13,6 +14,7 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final GoogleSignIn _googleSignIn = GoogleSignIn();
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  bool _isButtonDisabled = false;
 
   @override
   Widget build(BuildContext context) {
@@ -23,54 +25,81 @@ class _LoginPageState extends State<LoginPage> {
         ),
       ),
       body: Center(
-        child: RaisedButton(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(30.0),
-          ),
-          color: Colors.red,
-          child: Text('Login with Google'),
-          onPressed: _logIn,
-        ),
+        child: Builder(builder: (BuildContext context) {
+          return RaisedButton(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30.0),
+            ),
+            color: Colors.red,
+            child: _isButtonDisabled
+                ? Text('Logging in...')
+                : Text('Login with Google'),
+            onPressed: _isButtonDisabled ? null : () => _logIn(context),
+          );
+        }),
       ),
     );
   }
 
-  void _logIn() async {
+  void _logIn(BuildContext context) async {
+    setState(() {
+      _isButtonDisabled = true;
+    });
     GoogleSignInAccount googleUser;
     try {
       googleUser = await _googleSignIn.signIn();
     } catch (e) {
-      print(' \n');
-      print(e);
+      _errorHandle(context, 'An error occurred signing in to Google.');
+      return;
     }
-    GoogleSignInAuthentication googleAuth = await googleUser.authentication;
 
+    GoogleSignInAuthentication googleAuth = await googleUser.authentication;
     AuthCredential credential = GoogleAuthProvider.getCredential(
       accessToken: googleAuth.accessToken,
       idToken: googleAuth.idToken,
     );
 
-    FirebaseUser user = (await _auth.signInWithCredential(credential)).user;
+    AuthResult result;
+    try {
+      result = await _auth.signInWithCredential(credential);
+    } catch (e) {
+      _errorHandle(context, 'An error occurred signing in to the server.');
+      return;
+    }
 
-    if (user == null) return;
+    if (result.user == null) {
+      _errorHandle(context, 'Something went wrong.');
+      return;
+    }
 
     // Save to database if it is a new user
     List<DocumentSnapshot> documents = (await Firestore.instance
             .collection('users')
-            .where('id', isEqualTo: user.uid)
+            .where('id', isEqualTo: result.user.uid)
             .getDocuments())
         .documents;
     if (documents.isEmpty) {
-      Firestore.instance.collection('users').document(user.uid).setData({
-        'name': user.displayName,
-        'photoUrl': user.photoUrl,
-        'uid': user.uid,
-        'email': user.email,
+      Firestore.instance.collection('users').document(result.user.uid).setData({
+        'name': result.user.displayName,
+        'photoUrl': result.user.photoUrl,
+        'uid': result.user.uid,
+        'email': result.user.email,
       });
     }
 
     Navigator.of(context).pushReplacement(
       MaterialPageRoute(builder: (_) => HomePage()),
     );
+  }
+
+  void _errorHandle(BuildContext context, String msg) {
+    String checkConnection = 'Check your internet connection and try again.';
+    Scaffold.of(context).showSnackBar(SnackBar(
+      content: Text('$msg\n$checkConnection'),
+    ));
+
+    setState(() {
+      _isButtonDisabled = false;
+    });
   }
 }
