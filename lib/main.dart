29b51ace26c49741e50ab750222feb563c5b580b
Menno <menno1337@gmail.com>
2020-04-14 15:29:36 +0200

    Check if the user is already logged in

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,32 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+import 'package:kitley/pages/home_page.dart';
 import 'package:kitley/pages/login_page.dart';
 
-void main() => runApp(MaterialApp(
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       title: 'Kitley',
       theme: ThemeData(
         primarySwatch: Colors.orange,
       ),
-      home: LoginPage(),
-    ));
+      home: FutureBuilder(
+        future: FirebaseAuth.instance.currentUser(),
+        builder: (_, AsyncSnapshot<FirebaseUser> snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting:
+              return Center(child: Text('Loading....'));
+            default:
+              if (snapshot.hasError) return Text('Error: ${snapshot.error}');
+              if (snapshot.hasData) return HomePage();
+              return LoginPage();
+          }
+        },
+      ),
+    );
+  }
+}
