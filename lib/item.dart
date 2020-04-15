a92497b6e20a8dc1831cbccec8e75b34f5339205
Menno <menno1337@gmail.com>
2020-04-15 17:20:35 +0200

    Deduplication refactor

--- lib/item.dart
+++ lib/item.dart
@@ -1,8 +1,23 @@
+import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:geolocator/geolocator.dart';
 
 class Item {
   Item() {
     timestamp = Timestamp.now();
+  }
+
+  Item.fromDocumentSnapshot(DocumentSnapshot document) {
+    name = document['name'];
+    brand = document['brand'];
+    category = document['category'];
+    owner = document['owner'];
+    latitude = document['item_position'].latitude;
+    longitude = document['item_position'].longitude;
+    penalty = document['penalty'];
+    possessor = document['possessor'];
+    remarks = document['remarks'];
+    timestamp = document['timestamp'];
   }
 
   String brand;
@@ -28,5 +43,48 @@ class Item {
       'remarks': remarks,
       'timestamp': timestamp,
     };
+  }
+
+  Future<String> distanceToItem() async {
+    bool locationEnabled = await Geolocator().isLocationServiceEnabled();
+    if (!locationEnabled) return 'Enable your location service';
+
+    bool permissionGranted = GeolocationStatus.granted ==
+        await Geolocator().checkGeolocationPermissionStatus();
+    if (!permissionGranted) return 'Check your location permissions';
+
+    Position position = await Geolocator().getCurrentPosition();
+    double distance = await Geolocator().distanceBetween(
+      position.latitude,
+      position.longitude,
+      latitude,
+      longitude,
+    );
+
+    // distance is in meters
+    if (distance < 1000) return '${distance.toStringAsFixed(0)} meter';
+    distance /= 1000;
+    // distance is now in kilometers
+    if (distance < 20) return '${(distance).toStringAsFixed(1)} km';
+    return '${(distance).toStringAsFixed(0)} km';
+  }
+
+  Widget toWidget(void Function() onTap) {
+    return Card(
+      child: ListTile(
+        leading: CircleAvatar(),
+        title: Text(name),
+        subtitle: FutureBuilder(
+          initialData: 'Loading...',
+          future: distanceToItem(),
+          builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+            if (snapshot.hasError) return Text('Error: ${snapshot.error}');
+
+            return Text(snapshot.data);
+          },
+        ),
+        onTap: onTap,
+      ),
+    );
   }
 }
