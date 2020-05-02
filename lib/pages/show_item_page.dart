3485b98be3f228faec082e87fb1cdb408671cb45
Menno <menno1337@gmail.com>
2020-05-02 23:24:48 +0200

    add button to add chat (partner)

--- lib/pages/show_item_page.dart
+++ lib/pages/show_item_page.dart
@@ -1,6 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import 'package:kitley/utils/item.dart';
+import 'package:kitley/utils/user.dart';
 
 class ShowItemPage extends StatelessWidget {
   final Item _item;
@@ -12,8 +15,40 @@ class ShowItemPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(_item.name),
+      appBar: AppBar(title: Text(_item.name)),
+      body: FutureBuilder(
+        future: FirebaseAuth.instance.currentUser(),
+        builder: (_, AsyncSnapshot<FirebaseUser> snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting:
+              return Center(child: Text('Loading....'));
+            default:
+              if (snapshot.hasError) return Text('Error: ${snapshot.error}');
+
+              User myUser = User.fromFireBaseUser(snapshot.data);
+              return _buildButton(myUser);
+          }
+        },
+      ),
+    );
+  }
+
+  Widget _buildButton(User myUser) {
+    return Center(
+      child: RaisedButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+        color: Colors.red,
+        child: Text('Chat!'),
+        onPressed: () {
+          Firestore.instance
+              .collection('users')
+              .document(myUser.uid)
+              .collection('chats')
+              .document(_item.owner)
+              .setData({});
+        },
       ),
     );
   }
